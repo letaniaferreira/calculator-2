de00ca0fdea8679a1ff4bdef5f8377508867f93a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-24 17:28:58 -0700

    added spaces between elif statments moved quit break to the top of loop

--- calculator.py
+++ calculator.py
@@ -11,29 +11,41 @@ from arithmetic import *
 
 while True:
     ops_and_nums = raw_input("> ")
-    ops_and_nums = ops_and_nums.split(" ")
+    ops_and_nums = ops_and_nums.split()
     operator = ops_and_nums[0]
-    if len(ops_and_nums) >= 2:
-        first_num = int(ops_and_nums[1])
-    if len(ops_and_nums) == 3:
-        second_num = int(ops_and_nums[2])
+
     if operator == "q":
         break
+
+    if len(ops_and_nums) >= 2:
+        first_num = int(ops_and_nums[1])
+
+    if len(ops_and_nums) == 3:
+        second_num = int(ops_and_nums[2])
+
     elif operator == "+":
         print add(first_num, second_num)
+
     elif operator == "-":
         print subtract(first_num, second_num)
+
     elif operator == "*":
         print multiply(first_num, second_num)
+
     elif operator == "/":
         print divide(first_num, second_num)
+
     elif operator == "square":
         print square(first_num)
+
     elif operator == "cube":
         print cube(first_num)
+
     elif operator == "pow":
         print power(first_num, second_num)
+
     elif operator == "mod":
         print mod(first_num, second_num)
+        
     else:
         print "That input is invalid, please use a valid operator and integers"
